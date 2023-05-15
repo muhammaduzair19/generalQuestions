388664fe5ba6758691d8ed3362a2d3fba354fb01
muhammaduzair19 <muhammaduzairilyas@gmail.com>
2023-05-15 01:05:28 -0700

    general Question Project completed

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+function showAns1(){
+    const ans = document.getElementById('ans-1')
+    ans.classList.toggle('hidden');
+    const icon = document.getElementById('icon1')
+    icon.classList.toggle('fa-chevron-down');
+    icon.classList.toggle('fa-chevron-up');
+}
+
+function showAns2(){
+    const ans = document.getElementById('ans-2')
+    ans.classList.toggle('hidden');
+    const icon = document.getElementById('icon2')
+    icon.classList.toggle('fa-chevron-down');
+    icon.classList.toggle('fa-chevron-up');
+}
+
+function showAns3(){
+    const ans = document.getElementById('ans-3')
+    ans.classList.toggle('hidden');
+    const icon = document.getElementById('icon3')
+    icon.classList.toggle('fa-chevron-down');
+    icon.classList.toggle('fa-chevron-up');
+}
